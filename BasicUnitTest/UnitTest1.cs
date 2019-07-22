e7d4c301023a052c7d47b1dd0e99b271c41c3ec8
Heena Sood <Heena.Sood@infotools.com>
2019-07-22 16:19:34 +1200

    updated testcase

--- BasicUnitTest/UnitTest1.cs
+++ BasicUnitTest/UnitTest1.cs
@@ -11,32 +11,18 @@ namespace BasicUnitTest
 {
     [TestClass]
     public class UnitTest1
-    { 
+    {
         [TestMethod]
         public void Test_Login()
         {
             bool expected_result = true;
             bool actual_result = DataAccessLayer.Equals("admin", "admin");
             Assert.AreEqual(expected_result, actual_result, "Test is passed");
+            Console.WriteLine("UNIT TEST IS PASSED");
         }
 
-        private TimeSpan Time(Action toTime)
-        {
-            var timer = Stopwatch.StartNew();
-            toTime();
-            timer.Stop();
-            return timer.Elapsed;
-        }
-
-        [TestMethod]
-        public void PerformanceSpeedEnd(Stopwatch stopwatch)
-        {
-            stopwatch.Stop();
-            TimeSpan ts = stopwatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("Run Time: " + elapsedTime);
-        }
+       
     }
 
+    
 }
-
